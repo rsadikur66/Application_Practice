527a36e31898023e4dacb38e4d66513ab7adeefa
Mohammad Sadikur Rahman <32734077+rsadikur66@users.noreply.github.com>
2018-09-11 09:43:34 +0600

    welcome

---
+++ WebApplication_Practice_Web/WebForm1.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication_Practice_Web.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:DropDownList runat="server" ID="ddlCity"  />
+</asp:Content>
---
+++ WebApplication_Practice_Web/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication_PracticeDAL.DAL;
+
+namespace WebApplication_Practice_Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private readonly T0001Dal t0001Dal = new T0001Dal();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ddlCity.DataSource = t0001Dal.GetddlCity();
+            ddlCity.DataTextField ="CITY_NAME";
+            ddlCity.DataValueField = "CITY_ID";
+            ddlCity.DataBind();
+        }
+    }
+}
